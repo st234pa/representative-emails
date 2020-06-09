0b94285fda3744896f51f6ea16339970187c77d7
st234pa <stephanie.yoon123@gmail.com>
2020-06-09 19:54:40 -0400

    Updates

---
+++ precache-manifest.d5faffd253b7f1435e489e3311525fa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bc96d404edd5a1b917441064dc7957",
+    "url": "/representative-emails/index.html"
+  },
+  {
+    "revision": "e40150876ed2811614bd",
+    "url": "/representative-emails/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fc4683088276750afa0b",
+    "url": "/representative-emails/static/css/main.066b42f0.chunk.css"
+  },
+  {
+    "revision": "e40150876ed2811614bd",
+    "url": "/representative-emails/static/js/2.f4517e85.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/representative-emails/static/js/2.f4517e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4683088276750afa0b",
+    "url": "/representative-emails/static/js/main.d600c9f4.chunk.js"
+  },
+  {
+    "revision": "0cf34c4f2e8720241878",
+    "url": "/representative-emails/static/js/runtime-main.426fcf57.js"
+  }
+]);
